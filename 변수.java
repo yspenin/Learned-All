7cd1e435a2aa82f23a97741a0b685d56c8cd93e4
yspenin <yuseong14@gmail.com>
2023-09-04 12:41:00 +0900

    20230904

---
+++ 변수.java
@@ -0,0 +1,22 @@
+변수
+	Data를 저장하는 공간
+
+변수 초기화
+	변수에 data를 저장하는 것
+
+변수의 종류
+	class, instance, local
+
+변수 type
+	Data 값에 따라 결정, 반드시 Data type과 일치하는 변수 선언
+		문자 - char
+		정수 - byte, short, int, long
+		실수 - float, double
+		논리 - boolean - true, false
+
+상수
+	한 번만 Data에 값 저장
+
+리터럴
+	Data 값 자체
+
