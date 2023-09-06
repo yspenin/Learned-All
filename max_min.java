e5ca4bd3364778a1ccdba0c89946becb3db85ec2
yspenin <yuseong14@gmail.com>
2023-09-06 15:05:48 +0900

    230906

--- max_min.java
+++ max_min.java
@@ -3,12 +3,23 @@ public class max_min {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	final int n = (int)(Math.random()*100);
-	int[] a = new int[n];
-	for(int i = 1;i<=n;i++){
-		a[i-1] = (int)(Math.random()*1000);
-	}
+		final int n = (int)(Math.random()*99)+1;
+		int[] a = new int[n];
+		for(int i = 0;i<n;i++){
+			a[i] = (int)(Math.random()*1000);
+		}
 	String A = Arrays.toString(a);
-	System.out.print(A);
+	System.out.println(A);
+		int max = 0;
+		int min = 1000;
+		for(int i = 0; i < n; i++){			
+			 if(a[i]>max){
+				 max=a[i];
+			 }if(a[i]<min){
+				 min=a[i];
+			 }
+		}
+		System.out.println(max);
+		System.out.println(min);
 	}	
 }		
